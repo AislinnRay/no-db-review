79fabc941f00cca395a4c731ec9aaaac7b4a9573
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-21 15:31:12 -0600

    almost done

--- src/components/Grass.js
+++ src/components/Grass.js
@@ -11,15 +11,18 @@ class Grass extends Component {
     checkGrass() {
         const {data} = this.props
         this.props.catchPokemon(data.name, data.sprites.front_default)
+        this.props.refreshFn()
     }
 
-    render() {
+    render () {
         return (
-        <div>
-            <img scr={this.props.data.sprites.front_default} 
-            alt={this.props.data.name}
-            onClick={()=> this.checkGrass()}/>
-            <p>{this.props.data.name}</p>
+            <div>
+                <img
+                    src={this.props.data.sprites.front_default}
+                    alt={this.props.data.name}
+                    onClick={() => this.checkGrass()}
+                />
+                <p>{this.props.data.name}</p>
             </div>
         )
     }
